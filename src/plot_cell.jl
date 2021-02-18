d39be7468208249bbf3474caac34f54d6eb59bb6
Ali Ghorashi <aligho@mit.edu>
2021-02-18 15:10:24 -0500

    added stuff for im_pol(q, w)

--- src/plot_cell.jl
+++ src/plot_cell.jl
@@ -124,18 +124,31 @@ function plot_lattice(lattice_file::String)
 end
 
 "returns the imaginary value of the polarization at frequency omega (eV) and wavevector q (inverse angstrom)"
-function im_polarization(wannier_file::String, cell_map_file::String, lattice_vectors::Array{Array{Q, 1},1}, q::Array{T, 1}, ω::R, μ::S; spin=2, mesh=100, histogram_width=100) where {T<:Number, R<:Number, Q<:Number, S<:Number}
+function im_polarization(wannier_file::String, cell_map_file::String, lattice_vectors::Array{Array{Q, 1},1}, q::Array{T, 1}, μ::S; spin=2, mesh=100, histogram_width=100) where {T<:Number, Q<:Number, S<:Number}
     
+    Polarization_Array=zeros(histogram_width*100)
+
+    V=(2π)^2/brillouin_zone_area(lattice_vectors)
+    np=pyimport("numpy")
     qnormalized = normalize_kvector(lattice_vectors, q)
+
     for i in 1:mesh
         for j in 1:mesh
             kvector=[i/mesh, j/mesh, 0]
-            E1=plot_wannier_bands(wannier_file, cell_map_file, kvector  )
-            E2=plot_wannier_bands(wannier_file, cell_map_file, kvector  )
+            E1=wannier_bands(wannier_file, cell_map_file, kvector  )
+            E2=wannier_bands(wannier_file, cell_map_file, kvector+qnormalized  )
+            
+            f1=np.heaviside( μ-E1, 0.5)
+            f2=np.heaviside( μ-E2, 0.5)
+
+            DeltaE=E2-E1
+            if DeltaE>0
+                Polarization_Array[round(Int, histogram_width*DeltaE+1)] = Polarization_Array[round(Int, histogram_width*DeltaE+1)]+π*(f2-f1)/V*(1/mesh)^2*histogram_width
+            end
         end
     end
 
-    return spin
+    return Polarization_Array
 end
 
 function plot_bands(band_file::String, num_bands::Int, num_points::Int)
