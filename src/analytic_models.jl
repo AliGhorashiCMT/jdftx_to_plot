21631bd1aed74f8de28beb6dec5d06b47a9b8c7b
Ali Ghorashi <aligho@mit.edu>
2021-02-18 23:13:48 -0500

    added analytic model stuff

---
+++ src/analytic_models.jl
@@ -0,0 +1,25 @@
+using Plots
+
+function show_analytic_model(f::Function)
+    plot(-10:0.1:10, f.(-10:0.1:10), linewidth=5)
+end
+
+function analytic_dos(f::Function; mesh=100, histogram_width=100, max_energy=10)
+
+    dos_array = zeros(histogram_width*max_energy)
+    for x_iter in 1:mesh
+        for y_iter in 1:mesh
+            x = x_iter/mesh*100
+            y = y_iter/mesh*100
+            energy = f(x, y)
+
+            if energy<max_energy
+                dos_array[round(Int, energy*histogram_width)] = dos_array[round(Int, energy*histogram_width)] + histogram_width/mesh^2
+            end
+
+        end
+    end
+
+    return dos_array
+
+end
