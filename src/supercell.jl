8671d7d7f9dc946e4a7170a11a94014b27d73024
Ali Ghorashi <aligho@mit.edu>
2021-02-25 13:15:41 -0500

    added defectcell function

--- src/supercell.jl
+++ src/supercell.jl
@@ -24,3 +24,12 @@ function make_supercell(small_lattice::lattice, small_ionpos::ionpos, cell_mult:
 
     return lattice([small_lattice.rvectors[:, 1]*cell_mult[1] small_lattice.rvectors[:, 2]*cell_mult[2] small_lattice.rvectors[:, 3]*cell_mult[3]]), supercell_ionpos 
 end
+
+function make_defectcell(small_lattice::lattice, small_ionpos::ionpos, cell_mult::Array{Int, 1}, defect_atom::String)
+
+    defect_lattice, supercell_ionpos =  make_supercell(small_lattice, small_ionpos, cell_mult)
+    supercell_ionpos[1][2] = defect_atom
+
+    return defect_lattice, supercell_ionpos
+
+end
