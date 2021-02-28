47af278570629146ca63d5e49c60f1f8b03eaf3e
Ali Ghorashi <aligho@mit.edu>
2021-02-28 18:12:05 -0500

    added another method for im_polarization

--- src/susceptibility_from_wannier.jl
+++ src/susceptibility_from_wannier.jl
@@ -33,6 +33,36 @@ function im_polarization(wannier_file::String, cell_map_file::String, lattice_ve
     return Polarization_Array
 end
 
+function im_polarization(wannier_file::String, cell_map_file::String, lattvectors::lattice, q::Array{T, 1}, μ::S; spin=1, mesh=100, histogram_width=100) where {T<:Number, Q<:Number, S<:Number}
+    
+    Polarization_Array=zeros(histogram_width*100)
+
+    lattice_vectors = [lattvectors.rvectors[:, 1], lattvectors.rvectors[:, 2], lattvectors.rvectors[:, 3]]
+
+    V=(2π)^2/brillouin_zone_area(lattice_vectors)
+    np=pyimport("numpy")
+    qnormalized = normalize_kvector(lattice_vectors, q)
+
+    for i in 1:mesh
+        for j in 1:mesh
+            kvector=[i/mesh, j/mesh, 0]
+            E1=wannier_bands(wannier_file, cell_map_file, kvector  )
+            E2=wannier_bands(wannier_file, cell_map_file, kvector+qnormalized  )
+            
+            f1=np.heaviside( μ-E1, 0.5)
+            f2=np.heaviside( μ-E2, 0.5)
+
+            DeltaE=E2-E1
+            if DeltaE>0
+                Polarization_Array[round(Int, histogram_width*DeltaE+1)] = Polarization_Array[round(Int, histogram_width*DeltaE+1)]+π*(f2-f1)/V*(1/mesh)^2*histogram_width*spin
+            end
+        end
+    end
+
+    return Polarization_Array
+end
+
+
 
 function im_polarization(wannier_file::String, cell_map_file::String, nbands::Int, valence_bands::Int, lattice_vectors::Array{Array{Q, 1},1}, q::Array{T, 1}, μ::S; spin=1, mesh=100, histogram_width=100) where {T<:Number, Q<:Number, S<:Number}
     
@@ -72,7 +102,12 @@ function im_polarization(wannier_file::String, cell_map_file::String, nbands::In
     return Polarization_Array
 end
 
-
+function im_polarization(wannier_file_up::String, wannier_file_dn:String,  cell_map_file_up::String, cell_map_file_dn::String, nbands::Int, valence_bands_up::Int, valence_bands_dn::Int, lattice_vectors::Array{Array{Q, 1},1}, q::Array{T, 1}, μ::S; kwargs...)
+    #Here we add the independent polarizations from different spin channels 
+    spin_up_pol = im_polarization(wannier_file_up, cell_map_file_up, nbands, valence_bands_up, lattice_vectors, q, μ; kwargs... )
+    spin_dn_pol = im_polarization(wannier_file_dn, cell_map_file_dn, nbands, valence_bands_dn, lattice_vectors, q, μ; kwargs... )
+    return (spin_up_pol + spin_dn_pol)
+end
 
 "Applies the kramers-kronig relations onto a 1 dimensional array of numbers consisting of the imaginary value of the polarization to return the real value of polarization"
 function kramers_kronig(ω::T, im_pol::Array{R, 1}, max_energy::S, histogram_width::Q) where {T<:Number, R<:Number, Q<:Number, S<:Number}
