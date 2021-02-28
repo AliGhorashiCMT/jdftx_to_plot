ab750495163d75890fc7fcf2e8a5aa9d4c5e4cb4
Ali Ghorashi <aligho@mit.edu>
2021-02-27 20:50:05 -0500

    generalized wannier_bands

--- src/band_structures.jl
+++ src/band_structures.jl
@@ -20,7 +20,8 @@ end
 function wannier_bands(wannier_file::String, cell_map_file::String, k::Array{T, 1}) where T<:Number
     np=pyimport("numpy")
     cell_map=np.loadtxt(cell_map_file)
-    Hwannier=permutedims(reshape(np.loadtxt(wannier_file), (43, 1, 1)), [1, 3, 2])
+    cell_map_numlines=countlines(cell_map_file)
+    Hwannier=permutedims(reshape(np.loadtxt(wannier_file), (cell_map_numlines, 1, 1)), [1, 3, 2])
     phase = np.exp(2im*np.pi*cell_map*k); H = np.tensordot(phase, Hwannier, axes=1); E, U=np.linalg.eigh(H);
     return E[1]/eV 
 end
