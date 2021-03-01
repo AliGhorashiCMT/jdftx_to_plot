726b35140226c5987406261e2809570890c03fb2
Ali Ghorashi <aligho@mit.edu>
2021-03-01 15:45:39 -0500

    finished first_order_damping

--- src/loss_calculations/plasmon_losses.jl
+++ src/loss_calculations/plasmon_losses.jl
@@ -20,9 +20,39 @@ function landau_damping(wannier_file::String, cell_map_file::String, lattice_vec
     return lossarray
 end
 
-function first_order_damping()
-    return 2π/ħ
+function first_order_damping(wannier_file::String, cell_map_file::String, lattice_vectors::Array{Array{S, 1}, 1}, q::Array{T, 1}, μ::R, ϵphonon, gph; histogram_length=100, mesh=30, energy_range=10) where {T<:Number, R<:Number, S<:Number}
+    lossarray = zeros(histogram_width*energy_range)
+    qabs = sqrt(sum(q.^2))
+    qnormalized = normalize_kvector(lattice_vectors, q)
+    for xmesh in 1:mesh
+        for ymesh in 1:mesh
+            ϵinitial = wannier_bands(wannier_file, cell_map_file, [xmesh/mesh, ymesh/mesh, 0])
+            ϵmiddle = wannier_bands(wannier_file, cell_map_file, [xmesh/mesh, ymesh/mesh, 0]+qnormalized)        
+
+            finitial = ϵinitial<μ ? 1 : 0
+            fmiddle1 = ϵmiddle>μ ? 1 : 0
+            for xmesh1 in 1:mesh
+                for ymesh1 in 1:mesh
+
+                    ϵmiddle2 = wannier_bands(wannier_file, cell_map_file, [xmesh/mesh, ymesh/mesh, 0]+[xmesh1/mesh, ymesh1/mesh, 0])        
+                    fmiddle2 = ϵmiddle2>μ ? 1 : 0
+
+                    ϵfinal = wannier_bands(wannier_file, cell_map_file, [xmesh/mesh, ymesh/mesh, 0]+qnormalized+[xmesh1/mesh, ymesh1/mesh, 0])        
+                    
+                    ffinal = ϵfinal>μ ? 1 : 0
+
+                    ω = ϵfinal-ϵinitial+ϵphonon
+                    if ω>0
+                        lossarray[round(Int, ω*histogram_length+1)] = lossarray[round(Int, ω*histogram_length + 1 )] + (1/(ϵmiddle-ϵinitial-ω)+1/(ϵmiddle2-ϵinitial+ϵphonon))^2*gph^2*2π/ħ*e²ϵ/4*ω/qabs*finitial*fmiddle1*ffinal*(1/mesh)^4*histogram_length
+                    end
+                end
+            end
+        end
+    end
+    return lossarray
 end
+
+
 
 function second_order_damping()
     return 2π/ħ
