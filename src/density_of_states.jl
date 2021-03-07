53e1389d0c2e747644c39fbd78635a165a954898
Ali Ghorashi <aligho@mit.edu>
2021-03-07 17:58:14 -0500

    added more dos, chem potential methods

--- src/density_of_states.jl
+++ src/density_of_states.jl
@@ -6,7 +6,6 @@ using HCubature
 
 function density_of_states(dosfile_1::String, dosfile_2::String; kwargs... )
    
-    np=pyimport("numpy")
     plot(np.loadtxt(dosfile_1)[:, 1]*27.2, np.loadtxt(dosfile_1)[:, 2]/27.2, linewidth=4, size=(800, 400), xlims = (-2,-0.5), ylims = (0,500/27.2), label="Spin Up"; kwargs...)
     plot!(np.loadtxt(dosfile_2)[:, 1]*27.2, np.loadtxt(dosfile_2)[:, 2]/27.2, linewidth=4,  size=(800, 400), label="Spin Down"; kwargs...)
 
@@ -14,7 +13,6 @@ end
 
 function density_of_states(dosfile_1::String; kwargs...)
 
-    np=pyimport("numpy")
     plot(np.loadtxt(dosfile_1)[:, 1]*27.2, np.loadtxt(dosfile_1)[:, 2]/27.2, linewidth=4, size=(800, 400), xlims = (-2,-0.5), ylims = (0,500/27.2), label="Spin Unpolarized"; kwargs...)
 
 end
@@ -56,7 +54,7 @@ end
 
 
 function density_of_states_wannier(wannier_file::String, cell_map_file::String; mesh=100, histogram_width=100, energy_range=10, offset=0)
-    np=pyimport("numpy")
+
     WannierDOS=np.zeros(histogram_width*energy_range)
 
     for x_mesh in 1:mesh
@@ -72,14 +70,56 @@ function density_of_states_wannier(wannier_file::String, cell_map_file::String; 
 
 end
 
+function density_of_states_wannier(HWannier::Array{Float64, 3}, cell_map::Array{Float64, 2}; mesh=100, histogram_width=100, energy_range=10, offset=0)
+
+    WannierDOS=np.zeros(histogram_width*energy_range)
+
+    for x_mesh in 1:mesh
+        for y_mesh in 1:mesh
+            
+            ϵ=  wannier_bands(HWannier, cell_map, [x_mesh/mesh, y_mesh/mesh, 0])
+            WannierDOS[round(Int, histogram_width*(ϵ+offset))]=WannierDOS[round(Int, histogram_width*(ϵ+offset))]+histogram_width*(1/mesh)^2
+
+        end
+    end
+
+    return WannierDOS
+
+end
+
+
 function density_of_states_wannier(wannier_file::String, cell_map_file::String, nbands::Int; exclude_bands=[], mesh=100, histogram_width=100, energy_range=10, offset=0)
-    np=pyimport("numpy")
+
     WannierDOS=np.zeros(histogram_width*energy_range)
 
     for x_mesh in 1:mesh
         for y_mesh in 1:mesh
             
             ϵ=  wannier_bands(wannier_file, cell_map_file, [x_mesh/mesh, y_mesh/mesh, 0], nbands)
+            for band in 1:nbands
+                if band ∉ exclude_bands
+                    ϵ_band = ϵ[band]
+                    WannierDOS[round(Int, histogram_width*(ϵ_band+offset))]=WannierDOS[round(Int, histogram_width*(ϵ_band+offset))]+histogram_width*(1/mesh)^2
+            
+                end
+        
+            end
+    
+        end
+    end
+
+    return WannierDOS
+
+end
+
+function density_of_states_wannier(HWannier::Array{Float64, 3}, cell_map::Array{Float64, 2}, nbands::Int; exclude_bands=[], mesh=100, histogram_width=100, energy_range=10, offset=0)
+
+    WannierDOS=np.zeros(histogram_width*energy_range)
+
+    for x_mesh in 1:mesh
+        for y_mesh in 1:mesh
+            
+            ϵ=  wannier_bands(HWannier, cell_map, [x_mesh/mesh, y_mesh/mesh, 0], nbands)
             for band in 1:nbands
                 if band ∉ exclude_bands
                     ϵ_band = ϵ[band]
@@ -115,6 +155,25 @@ function find_chemical_potential(wannier_file::String, cell_map_file::String; me
 
 end
 
+function find_chemical_potential(HWannier::Array{Float64, 3}, cell_map::Array{Float64, 2}; mesh=100, histogram_width=100, energy_range=10, offset=0)
+    
+    doss = density_of_states_wannier(HWannier, cell_map, mesh=mesh, histogram_width=histogram_width, energy_range=energy_range, offset=offset )
+    totalstates = []
+    for i in 1:length(doss)
+        push!(totalstates, [i/histogram_width-offset, sum(doss[1:i]*1/histogram_width)])
+    end
+    xenergies = []
+    yoccupations = []
+    for i in 1:length(doss)
+        push!(xenergies, totalstates[i][1])
+        push!(yoccupations, totalstates[i][2])
+    end
+
+    return xenergies, yoccupations
+
+end
+
+
 function find_num_phonons(cell_map::String, phononOmegaSq::String; mesh=100, histogram_width=100, energy_range=2)
     
     doss = phonon_density_of_states(cell_map, phononOmegaSq; mesh=mesh, histogram_width=histogram_width, energy_range=energy_range)
@@ -137,7 +196,6 @@ end
 
 function density_of_states_wannier(wannier_file_up::String, cell_map_file_up::String, wannier_file_dn::String, cell_map_file_dn::String,; mesh=100, histogram_width=100, energy_range=10, offset=0)
    
-    np=pyimport("numpy")
     WannierDOSUp=np.zeros(histogram_width*energy_range)
     WannierDOSDn=np.zeros(histogram_width*energy_range)
 
@@ -161,7 +219,7 @@ end
 
 "Returns the phonon density of states (phonons per unit energy per unit cell)"
 function phonon_density_of_states(cell_map::String, phononOmegaSq::String; mesh=100, histogram_width=100, energy_range=2)
-    np=pyimport("numpy")
+
     PhononDOS=np.zeros(histogram_width*energy_range)
 
     for x_mesh in 1:mesh
