13dd0530d982b43a88f0574ac273287e8d782830
Ali Ghorashi <aligho@mit.edu>
2021-03-08 15:11:46 -0500

    docstrings for supercell and defectcell

--- src/supercell.jl
+++ src/supercell.jl
@@ -1,3 +1,33 @@
+
+"""
+Returns a supercell with the designated multiplicity from a smaller unit cell 
+
+```
+julia-repl
+julia> simple_cubic = lattice([1 0 0; 0 1 0; 0 0 1])
+julia> small_ionpos = ionpos([["ion", "C", 0, 0, 0, 1], ["ion", "C", 0.5, 0.5, 0.5, 1]])
+julia> supercell_ionpos = make_supercell(simple_cubic, small_ionpos, [2, 2, 2])[2]
+julia>  for ion in supercell_ionpos.ionpos 
+            println(ion)
+        end
+Any["ion", "C", 0.0, 0.0, 0.0, 1.0]
+Any["ion", "C", 0.0, 0.0, 0.5, 1.0]
+Any["ion", "C", 0.0, 0.5, 0.0, 1.0]
+Any["ion", "C", 0.0, 0.5, 0.5, 1.0]
+Any["ion", "C", 0.5, 0.0, 0.0, 1.0]
+Any["ion", "C", 0.5, 0.0, 0.5, 1.0]
+Any["ion", "C", 0.5, 0.5, 0.0, 1.0]
+Any["ion", "C", 0.5, 0.5, 0.5, 1.0]
+Any["ion", "C", 0.25, 0.25, 0.25, 1.0]
+Any["ion", "C", 0.25, 0.25, 0.75, 1.0]
+Any["ion", "C", 0.25, 0.75, 0.25, 1.0]
+Any["ion", "C", 0.25, 0.75, 0.75, 1.0]
+Any["ion", "C", 0.75, 0.25, 0.25, 1.0]
+Any["ion", "C", 0.75, 0.25, 0.75, 1.0]
+Any["ion", "C", 0.75, 0.75, 0.25, 1.0]
+Any["ion", "C", 0.75, 0.75, 0.75, 1.0]
+```
+"""
 function make_supercell(small_lattice::lattice, small_ionpos::ionpos, cell_mult::Array{Int, 1})
     
     mult1, mult2, mult3 = cell_mult
@@ -21,13 +51,43 @@ function make_supercell(small_lattice::lattice, small_ionpos::ionpos, cell_mult:
         end
     end
 
-    return lattice([small_lattice.rvectors[:, 1]*cell_mult[1] small_lattice.rvectors[:, 2]*cell_mult[2] small_lattice.rvectors[:, 3]*cell_mult[3]]), supercell_ionpos 
+    return lattice([small_lattice.rvectors[:, 1]*cell_mult[1] small_lattice.rvectors[:, 2]*cell_mult[2] small_lattice.rvectors[:, 3]*cell_mult[3]]), ionpos(supercell_ionpos) 
 end
 
+
+"""
+Returns a defect cell with the designated multiplicity from a smaller unit cell and a designated defect atom
+
+```
+julia-repl
+julia> simple_cubic = lattice([1 0 0; 0 1 0; 0 0 1])
+julia> small_ionpos = ionpos([["ion", "C", 0, 0, 0, 1], ["ion", "C", 0.5, 0.5, 0.5, 1]], "B")
+julia> defectcell_ionpos = make_supercell(simple_cubic, small_ionpos, [2, 2, 2])[2]
+julia>  for ion in defectcell_ionpos.ionpos
+            println(ion)
+        end
+Any["ion", "B", 0.0, 0.0, 0.0, 1.0]
+Any["ion", "C", 0.0, 0.0, 0.5, 1.0]
+Any["ion", "C", 0.0, 0.5, 0.0, 1.0]
+Any["ion", "C", 0.0, 0.5, 0.5, 1.0]
+Any["ion", "C", 0.5, 0.0, 0.0, 1.0]
+Any["ion", "C", 0.5, 0.0, 0.5, 1.0]
+Any["ion", "C", 0.5, 0.5, 0.0, 1.0]
+Any["ion", "C", 0.5, 0.5, 0.5, 1.0]
+Any["ion", "C", 0.25, 0.25, 0.25, 1.0]
+Any["ion", "C", 0.25, 0.25, 0.75, 1.0]
+Any["ion", "C", 0.25, 0.75, 0.25, 1.0]
+Any["ion", "C", 0.25, 0.75, 0.75, 1.0]
+Any["ion", "C", 0.75, 0.25, 0.25, 1.0]
+Any["ion", "C", 0.75, 0.25, 0.75, 1.0]
+Any["ion", "C", 0.75, 0.75, 0.25, 1.0]
+Any["ion", "C", 0.75, 0.75, 0.75, 1.0]
+```
+"""
 function make_defectcell(small_lattice::lattice, small_ionpos::ionpos, cell_mult::Array{Int, 1}, defect_atom::String)
 
     defect_lattice, supercell_ionpos =  make_supercell(small_lattice, small_ionpos, cell_mult)
-    supercell_ionpos[1][2] = defect_atom
+    supercell_ionpos.ionpos[1][2] = defect_atom
 
     return defect_lattice, supercell_ionpos
 
