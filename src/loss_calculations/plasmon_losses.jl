f18ed47c70497c0ece76f109f39deb280def2b23
Ali Ghorashi <aligho@mit.edu>
2021-03-09 11:12:04 -0500

    fixed first order damping

--- src/loss_calculations/plasmon_losses.jl
+++ src/loss_calculations/plasmon_losses.jl
@@ -90,7 +90,7 @@ function first_order_damping(HWannier::Array{Float64, 3}, cell_map::Array{Float6
                     ϵmiddle2 = wannier_bands(HWannier, cell_map, [xmesh/mesh, ymesh/mesh, 0]+[xmesh1/mesh, ymesh1/mesh, 0])        
                     fmiddle2 = ϵmiddle2>μ ? 1 : 0
 
-                    ϵfinal = wannier_bands(Hwannier, cell_map, [xmesh/mesh, ymesh/mesh, 0]+qnormalized+[xmesh1/mesh, ymesh1/mesh, 0])        
+                    ϵfinal = wannier_bands(HWannier, cell_map, [xmesh/mesh, ymesh/mesh, 0]+qnormalized+[xmesh1/mesh, ymesh1/mesh, 0])        
                     
                     ffinal = ϵfinal>μ ? 1 : 0
 
