acdb7c1fe280064a45819489ba6125146645e000
Ali Ghorashi <aligho@mit.edu>
2021-03-30 16:27:27 -0400

    bands for spin = 1

--- src/band_structures.jl
+++ src/band_structures.jl
@@ -11,15 +11,25 @@ that the spin degeneracy in jdftx is included in the number of k points- not the
 the k points from 1:num_points will be for one spin species and those from num_points+1 to 2*npoints
 correspond to the other spin species.
 """
-function plot_bands(band_file::String, num_bands::Int, num_points::Int; kwargs...)
+function plot_bands(band_file::String, num_bands::Int, num_points::Int; spin::Int=1, kwargs...)
 
-    reshaped=reshape(read!(band_file, Array{Float64}(undef, num_bands*num_points*2 )),(num_bands, num_points*2));
-    exactenergiesup=permutedims(reshaped, [2, 1])[1:num_points, :]*1/eV;
-    exactenergiesdown=permutedims(reshaped, [2, 1])[num_points+1:2*num_points, :]*1/eV;
+    if spin == 1
+        reshaped = reshape(read!(band_file, Array{Float64}(undef, num_bands*num_points )),(num_bands, num_points));
+        exactenergies = permutedims(reshaped, [2, 1])*1/eV;
 
-    plot(exactenergiesdown, color="black", label="", linewidth=2; kwargs...)
-    plot!(exactenergiesup, color="purple", label="", linewidth=2; kwargs...)
+        plot(exactenergies, color="black", label="", linewidth=2; kwargs...)
 
+    elseif spin ==2 
+
+        reshaped=reshape(read!(band_file, Array{Float64}(undef, num_bands*num_points*2 )),(num_bands, num_points*2));
+        exactenergiesup=permutedims(reshaped, [2, 1])[1:num_points, :]*1/eV;
+        exactenergiesdown=permutedims(reshaped, [2, 1])[num_points+1:2*num_points, :]*1/eV;
+
+        ##Note that Plots.jl automatically plots 2d arrays columnwise- which is why the band indices now correspond to column indices
+        plot(exactenergiesdown, color="black", label="", linewidth=2; kwargs...)
+        plot!(exactenergiesup, color="purple", label="", linewidth=2; kwargs...)
+
+    end
 end
 
 function wannier_bands(wannier_file::String, cell_map_file::String, k::Array{<:Real, 1}) 
